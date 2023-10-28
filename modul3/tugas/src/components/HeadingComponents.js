25165999382182b85536d1ef2cfd05e4917ce973
BosToken <faizdiandra11@gmail.com>
2023-10-28 10:34:09 +0700

    assets

--- modul3/tugas/src/components/HeadingComponents.js
+++ modul3/tugas/src/components/HeadingComponents.js
@@ -24,6 +24,14 @@ function HeadingComponents() {
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
         crossorigin="anonymous"
       />
+
+      {/* font */}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap"
+        rel="stylesheet"
+      ></link>
     </>
   );
 }
--- modul3/tugas/src/pages/Landing.js
+++ modul3/tugas/src/pages/Landing.js
@@ -1,6 +1,7 @@
 import "../components/HeadingComponents";
 import Container from "react-bootstrap/Container";
 import NavbarLandingComponent from "../components/NavbarLandingComponents";
+import CardLandingComponents from "../components/CardLandingComponents";
 import CarouselComponents from "../components/CarouselComponents";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -17,18 +18,72 @@ function Landing() {
         </Container>
       </div>
       <div className="p-5" style={{ background: "#2E4E8B" }}>
-        <Row>
-          <Col>
-            Why Choose our Website
-            <p>
-              WaifuBapakmu provides a gacha character service with a high rating
-              so you can vent your trash of gacha
-            </p>
-          </Col>
-          <Col>
-            {/* <img src="../../public/image/holo.png"/> */}
-          </Col>
-        </Row>
+        <Container>
+          <Row>
+            <Col>
+              <div className="ms-5 mt-5">
+                <h1
+                  className="mb-3"
+                  style={{
+                    fontFamily: "Poppins",
+                    fontFamily: "sans-serif",
+                    color: "white",
+                  }}
+                >
+                  <b>Why Choose our Website</b>
+                </h1>
+                <h2
+                  style={{
+                    fontFamily: "Poppins",
+                    fontFamily: "sans-serif",
+                    color: "white",
+                  }}
+                >
+                  WaifuBapakmu provides a gacha character service with a high
+                  rating so you can vent your trash of gacha.
+                </h2>
+              </div>
+            </Col>
+            <Col>
+              <center>
+                <img
+                  src="https://raw.githubusercontent.com/FaizDiandraMaulana262/pemrograman_web/master/modul3/tugas/public/image/holo.png"
+                  width="150"
+                />
+              </center>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="p-5" style={{ background: "#639CD9" }}>
+        <Container>
+          <center>
+            <h1
+              style={{
+                color: "white",
+                fontFamily: "Poppins",
+                fontFamily: "sans-serif",
+              }}
+              className="mb-5"
+            >
+              <b>Example Result</b>
+            </h1>
+            <Row>
+              <Col>
+                <CardLandingComponents />
+              </Col>
+              <Col>
+                <CardLandingComponents />
+              </Col>
+              <Col>
+                <CardLandingComponents />
+              </Col>
+              <Col>
+                <CardLandingComponents />
+              </Col>
+            </Row>
+          </center>
+        </Container>
       </div>
     </>
   );
