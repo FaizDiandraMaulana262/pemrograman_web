515f51843c0dafd53a1c0e9e50f1d08065d5edfc
BosToken <faizdiandra11@gmail.com>
2023-10-28 09:39:00 +0700

    holo

--- modul3/tugas/src/components/NavbarLandingComponents.js
+++ modul3/tugas/src/components/NavbarLandingComponents.js
@@ -1,20 +1,31 @@
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import Container from "react-bootstrap/Container";
+import Button from 'react-bootstrap/Button';
 
 function NavbarLandingComponent() {
   return (
     <>
-      <Navbar className="bg-body-tertiary">
+      <Navbar style={{background: "#342056"}}>
         <Container>
           <Navbar.Brand href="#">
           <img
-              src=""
-              width="30"
-              height="30"
+              src="https://raw.githubusercontent.com/FaizDiandraMaulana262/pemrograman_web/master/modul3/tugas/public/image/dad1.png"
+              width="40"
+              height="40"
               className="d-inline-block align-top"
-              alt="React Bootstrap logo"
+              alt="<React Bootstrap logo"
             />
           </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home"><b style={{color: "white"}}>Home</b></Nav.Link>
+            <Nav.Link href="#features"><b style={{color: "white"}}>Features</b></Nav.Link>
+            <Nav.Link href="#pricing"><b style={{color: "white"}}>Pricing</b></Nav.Link>
+          </Nav>
+          <Nav className="ms-auto">
+            <Nav.Link href="#home"><Button variant="success">Log In</Button></Nav.Link>
+            <Nav.Link href="#features"><Button variant="outline-info">Start Gacha</Button></Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
     </>
--- modul3/tugas/src/pages/Landing.js
+++ modul3/tugas/src/pages/Landing.js
@@ -1,14 +1,35 @@
 import "../components/HeadingComponents";
 import Container from "react-bootstrap/Container";
 import NavbarLandingComponent from "../components/NavbarLandingComponents";
+import CarouselComponents from "../components/CarouselComponents";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Landing() {
   return (
     <>
       <NavbarLandingComponent />
-      <Container className="m-3">
-        <div>jancok</div>
-      </Container>
+      <div style={{ background: "#639CD9" }}>
+        <Container>
+          <div className="py-5">
+            <CarouselComponents />
+          </div>
+        </Container>
+      </div>
+      <div className="p-5" style={{ background: "#2E4E8B" }}>
+        <Row>
+          <Col>
+            Why Choose our Website
+            <p>
+              WaifuBapakmu provides a gacha character service with a high rating
+              so you can vent your trash of gacha
+            </p>
+          </Col>
+          <Col>
+            {/* <img src="../../public/image/holo.png"/> */}
+          </Col>
+        </Row>
+      </div>
     </>
   );
 }
